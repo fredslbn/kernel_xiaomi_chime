076e8382635530a2a0b078d18a54145ace7cb50f
fredslbn <138670465+fredslbn@users.noreply.github.com>
2026-08-18 12:43:31 +0700

    Update module.c

--- kernel/module.c
+++ kernel/module.c
@@ -272,6 +272,11 @@ static void mod_update_bounds(struct module *mod)
 struct list_head *kdb_modules = &modules; /* kdb needs the list of modules */
 #endif /* CONFIG_KGDB_KDB */
 
+static void module_assert_mutex(void)
+{
+	lockdep_assert_held(&module_mutex);
+}
+
 static void module_assert_mutex_or_preempt(void)
 {
 #ifdef CONFIG_LOCKDEP
@@ -4240,7 +4245,9 @@ int module_kallsyms_on_each_symbol(int (*fn)(void *, const char *,
 	unsigned int i;
 	int ret;
 
-	mutex_lock(&module_mutex);
+    module_assert_mutex();
+	//mutex_lock(&module_mutex);
+
 	list_for_each_entry(mod, &modules, list) {
 		/* We hold module_mutex: no need for rcu_dereference_sched */
 		struct mod_kallsyms *kallsyms = mod->kallsyms;
