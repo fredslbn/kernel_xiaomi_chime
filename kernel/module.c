64ae610fefa6440836e17f39d1291921d15492a1
fredslbn <138670465+fredslbn@users.noreply.github.com>
2026-08-18 13:04:43 +0700

    Update module.c

--- kernel/module.c
+++ kernel/module.c
@@ -4246,7 +4246,9 @@ int module_kallsyms_on_each_symbol(int (*fn)(void *, const char *,
 	int ret;
 
     module_assert_mutex();
-	//mutex_lock(&module_mutex);
+	/* 
+	* mutex_lock(&module_mutex);
+	*/
 
 	list_for_each_entry(mod, &modules, list) {
 		/* We hold module_mutex: no need for rcu_dereference_sched */
